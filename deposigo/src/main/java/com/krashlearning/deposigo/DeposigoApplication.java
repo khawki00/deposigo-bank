a4c844bdfac79ebb2b79fc72eaff7aeb500f5c3b
Keesha Hawkins <khawki00@hotmail.com>
2025-10-25 17:48:22 -0400

    section 1

---
+++ deposigo/src/main/java/com/krashlearning/deposigo/DeposigoApplication.java
@@ -0,0 +1,13 @@
+package com.krashlearning.deposigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeposigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeposigoApplication.class, args);
+	}
+
+}
---
+++ deposigo/src/test/java/com/krashlearning/deposigo/DeposigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krashlearning.deposigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeposigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
